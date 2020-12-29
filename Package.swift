5b8803f610b37f8108c6a8aadb58bf360ba3cd8e
Vincenzo D'Ippolito <vincenzo.dippolito@lightstreamer.com>
2020-12-29 16:10:20 +0100

    Added linked dependencies

--- Package.swift
+++ Package.swift
@@ -14,11 +14,22 @@ let package = Package(
     products: [
         .library(
             name: "Lightstreamer_watchOS_Client", 
-            targets: ["Lightstreamer_watchOS_Client"]
+            targets: ["Lightstreamer_watchOS_Client_Wrapper"]
         )
     ],
     dependencies: [],
     targets: [
+        .target(
+            name: "Lightstreamer_watchOS_Client_Wrapper",
+            dependencies: [
+                .target(name: "Lightstreamer_watchOS_Client")
+            ],
+            path: "Sources",
+            linkerSettings: [
+                .linkedLibrary("iconv"),
+                .linkedFramework("Security")
+            ]
+        ),
         .binaryTarget(
             name: "Lightstreamer_watchOS_Client", 
             url: "https://www.lightstreamer.com/repo/cocoapods/ls-watchos-client/4.3.0/ls-watchos-client-4.3.0.zip", 
